2eaf39b12550c399d12c119ef1ea3698270dd69d
Nick Ganter <nickganter91@gmail.com>
2015-11-12 17:38:00 -0600

    champion information finished.

--- SummonerPlusApi/Controllers/ChampionController.cs
+++ SummonerPlusApi/Controllers/ChampionController.cs
@@ -15,7 +15,7 @@ namespace SummonerPlusApi.Controllers
 {
     public class ChampionController : ApiController
     {
-        public Dictionary<string, Champion> GetChampions()
+        public IHttpActionResult GetChampions()
         {
             string apiKey = ConfigurationManager.AppSettings["ApiKey"].ToString();
             using (var client = new HttpClient())
@@ -34,7 +34,38 @@ namespace SummonerPlusApi.Controllers
                         var jObj = JObject.Parse(jsonContent);
                         var data = (JObject)jObj["data"];
                         Dictionary<string, Champion> champions = JsonConvert.DeserializeObject<Dictionary<string, Champion>>(data.ToString());
-                        return champions;
+                        return Ok(champions);
+                    }
+                    catch (Exception ex)
+                    {
+                        string exception = ex.Message;
+                        return InternalServerError();
+                    }
+                }
+                return NotFound();
+            }
+        }
+
+        public Champion GetChampion(int championId)
+        {
+            string apiKey = ConfigurationManager.AppSettings["ApiKey"].ToString();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(@"https://global.api.pvp.net/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = client.GetAsync("api/lol/static-data/na/v1.2/champion?api_key=" + apiKey).Result;
+                if (response != null)
+                {
+                    try
+                    {
+                        HttpContent content = response.Content;
+                        string jsonContent = content.ReadAsStringAsync().Result;
+                        var jObj = JObject.Parse(jsonContent);
+                        var data = (JObject)jObj["data"];
+                        Dictionary<string, Champion> champions = JsonConvert.DeserializeObject<Dictionary<string, Champion>>(data.ToString());
+                        return champions.Values.FirstOrDefault();
                     }
                     catch (Exception ex)
                     {
@@ -45,36 +76,5 @@ namespace SummonerPlusApi.Controllers
                 return null;
             }
         }
-
-        //public Champion GetChampion(int championId)
-        //{
-        //    string apiKey = ConfigurationManager.AppSettings["ApiKey"].ToString();
-        //    using (var client = new HttpClient())
-        //    {
-        //        client.BaseAddress = new Uri(@"https://global.api.pvp.net/");
-        //        client.DefaultRequestHeaders.Accept.Clear();
-        //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-        //        var response = client.GetAsync("api/lol/static-data/na/v1.2/champion?api_key=" + apiKey).Result;
-        //        if (response != null)
-        //        {
-        //            try
-        //            {
-        //                HttpContent content = response.Content;
-        //                string jsonContent = content.ReadAsStringAsync().Result;
-        //                var jObj = JObject.Parse(jsonContent);
-        //                var data = (JObject)jObj["data"];
-        //                Dictionary<string, Champion> champions = JsonConvert.DeserializeObject<Dictionary<string, Champion>>(data.ToString());
-        //                return champions;
-        //            }
-        //            catch (Exception ex)
-        //            {
-        //                string exception = ex.Message;
-        //                return null;
-        //            }
-        //        }
-        //        return null;
-        //    }
-        //}
     }
 }
