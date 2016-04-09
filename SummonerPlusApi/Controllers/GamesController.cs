cf41d2d2f7240b6a3049c30b86c3040d19846fd9
Nick Ganter <nickganter94@hotmail.com>
2016-04-08 22:52:20 -0500

    some changes

---
+++ SummonerPlusApi/Controllers/GamesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SummonerPlusApi.Models;
+namespace SummonerPlusApi.Controllers
+{
+    public class GamesController : ApiController
+    {
+        [HttpGet, Route("api/Games/{summonerID}")]
+        public IHttpActionResult Games(long summonerID)
+        {
+            string apiKey = ConfigurationManager.AppSettings["ApiKey"].ToString();
+            return Ok(new List<Game>());
+        }
+    }
+}
